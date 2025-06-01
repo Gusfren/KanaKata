f4ecd4ca69c5df639853294be933e63766a30c2c
Gusfren <gusfren15@gmail.com>
2025-06-01 16:44:09 +0800

    gus1

---
+++ app/src/main/java/psti/unram/kanakata/HurufMenuActivity.kt
@@ -0,0 +1,35 @@
+package psti.unram.kanakata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HurufMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_huruf_menu)
+
+        val huruf = intent.getStringExtra("huruf") ?: "Huruf"
+        val hurufJepang = intent.getStringExtra("hurufJepang") ?: ""
+
+        val tvHuruf = findViewById<TextView>(R.id.tvHuruf)
+        val btnBelajar = findViewById<Button>(R.id.btnBelajar)
+        val btnKuis = findViewById<Button>(R.id.btnKuis)
+
+        tvHuruf.text = "$huruf\n$hurufJepang"
+
+        btnBelajar.setOnClickListener {
+            val intent = Intent(this, BelajarActivity::class.java)
+            intent.putExtra("huruf", huruf)
+            startActivity(intent)
+        }
+
+        btnKuis.setOnClickListener {
+            val intent = Intent(this, KuisActivity::class.java)
+            intent.putExtra("huruf", huruf)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/psti/unram/kanakata/MainMenu.kt
+++ app/src/main/java/psti/unram/kanakata/MainMenu.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main_menu)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnHiragana1 = findViewById<Button>(R.id.btnHiragana)
+        btnHiragana1.setOnClickListener {
+            val intent = Intent(this, HurufMenuActivity::class.java)
+            intent.putExtra("huruf", "Hiragana")
+            startActivity(intent)
+        }
+
+        val btnKatakana2 = findViewById<Button>(R.id.btnKatakana)
+        btnKatakana2.setOnClickListener {
+            val intent = Intent(this, HurufMenuActivity::class.java)
+            intent.putExtra("huruf", "Katakana")
+            startActivity(intent)
+        }
+
+        val btnKanji3 = findViewById<Button>(R.id.btnKanji)
+        btnKanji3.setOnClickListener {
+            val intent = Intent(this, HurufMenuActivity::class.java)
+            intent.putExtra("huruf", "Katakana")
+            startActivity(intent)
         }
     }
 }
